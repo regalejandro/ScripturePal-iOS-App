8f14f8728a672bd9501b1e0ca7d0f3d8b99ebf8a
regalejandro <abc.regalejandro.03@gmail.com>
2026-03-04 15:52:15 -0800

    Added ChapterRevealView to more dramtically reveal chapter selection

---
+++ ScriptureGo/Views/ChapterRevealView.swift
@@ -0,0 +1,18 @@
+//
+//  ChapterRevealView.swift
+//  ScriptureGo
+//
+//  Created by Alejandro Regalado on 3/4/26.
+//
+
+import SwiftUI
+
+struct ChapterRevealView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChapterRevealView()
+}
