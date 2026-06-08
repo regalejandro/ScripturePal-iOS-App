04fefa94e8bb4cc65a4c6c640894dc9e2aa8d258
regalejandro <abc.regalejandro.03@gmail.com>
2026-06-08 12:55:55 -0700

    Added HistoricalStatsView and refactored shared assests into new StatsComponents

---
+++ ScriptureGo/Views/HistoricalStatsView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoricalStatsView.swift
+//  ScriptureGo
+//
+//  Created by Alejandro Regalado on 6/8/26.
+//
+
+import SwiftUI
+
+struct HistoricalStatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoricalStatsView()
+}
